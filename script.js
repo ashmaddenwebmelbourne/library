713dc5816e5c2e14d94d40454932d9dbb6e9325b
ash-madden <ashleymadden55@gmail.com>
2022-06-16 02:54:09 +1000

    create add new book functionality

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
 let myLibrary = [];
+let btn = document.getElementById("submit-button");
+let cardContainer = document.getElementById("card-container")
+let book;
+let newDiv;
 
-function Book() {
-    // the constructor...
+
+function Book(title, author, rating, description) {
+    this.title = title;
+    this.author = author;
+    this.rating = rating;
+    this.description = description;
 }
 
-function addBookToLibrary() {
-    // do stuff here
-}
+btn.addEventListener("click", function(event) {
+    $('#Modal').modal('hide');
+    book = new Book(document.getElementById("title").value, document.getElementById("author").value, parseInt(document.getElementById("rating").value), document.getElementById("description").value);
+    myLibrary.push(book);
+    newDiv = document.createElement("div");
+    cardContainer.appendChild(newDiv);
+    newDiv.appendChild(document.createTextNode("Title: "));
+    newDiv.appendChild(document.createTextNode(book.title));
+    newDiv.appendChild(document.createElement("br"));
+    newDiv.appendChild(document.createTextNode("Author: "));
+    newDiv.appendChild(document.createTextNode(book.author));
+    newDiv.appendChild(document.createElement("br"));
+    newDiv.appendChild(document.createTextNode("Rating: "));
+    newDiv.appendChild(document.createTextNode(book.rating));
+    newDiv.appendChild(document.createElement("br"));
+    newDiv.appendChild(document.createTextNode("Description: "));
+    newDiv.appendChild(document.createTextNode(book.description));
+});
